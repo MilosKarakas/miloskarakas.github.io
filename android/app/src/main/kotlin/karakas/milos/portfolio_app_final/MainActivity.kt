44443fbb0097db07c6627f8f0b0fb44cdbc55ec0
Sara <sara.bojanic2402@gmail.com>
2020-01-22 01:10:55 +0100

    First commit from terminal for this project

---
+++ android/app/src/main/kotlin/karakas/milos/portfolio_app_final/MainActivity.kt
@@ -0,0 +1,12 @@
+package karakas.milos.portfolio_app_final
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Portfolio',
+      theme: ThemeData(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int _selectedLanguage = 0;
+  String _name = "Каракаш Милош";
+  String _centerText = "Израда у току.";
+
+  @override
+  Widget build(BuildContext context) {
+
+    if(_selectedLanguage != 0){
+      _name = "Karakaš Miloš";
+      _centerText = "Izrada u toku.";
+    }
+    else {
+      _name = "Каракаш Милош";
+      _centerText = "Израда у току.";
+    }
+
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 17, 49, 49),
+      appBar: AppBar(
+        backgroundColor: Colors.black12,
+        elevation: 0,
+        title: Text(
+          _name,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.w700
+          ),
+        ),
+        actions: <Widget>[
+          GestureDetector(
+            child: Container(
+              child: Center(
+                child: Text(
+                  "АБВ",
+                  style: TextStyle(
+                      color: (_selectedLanguage==0)?Colors.white:Color(0xffe7eaee),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 12
+                  ),
+                ),
+              ),
+              margin: EdgeInsets.all(6),
+            ),
+            onTap: (){
+              setState(() {
+                _selectedLanguage=0;
+              });
+            },
+          ),
+          GestureDetector(
+            child: Container(
+              child: Center(
+                child: Text(
+                  "ABC",
+                  style: TextStyle(
+                      color: (_selectedLanguage==1)?Colors.white:Color.fromARGB(255, 17, 49, 80),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 12
+                  ),
+                ),
+              ),
+              margin: EdgeInsets.all(6),
+            ),
+            onTap: (){
+              setState(() {
+                _selectedLanguage=1;
+              });
+            },
+          ),
+          GestureDetector(
+            child: Container(
+              child: Center(
+                child: Text(
+                  "ENG",
+                  style: TextStyle(
+                      color: (_selectedLanguage==2)?Colors.white:Color.fromARGB(255, 17, 49, 49),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 12
+                  ),
+                ),
+              ),
+              margin: EdgeInsets.only(top: 6, bottom: 6, left: 6, right: 12),
+            ),
+            onTap: (){
+              setState(() {
+                _selectedLanguage=2;
+              });
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: Text(
+          _centerText,
+          style: TextStyle(
+              color: Color.fromARGB(127, 255, 255, 255),
+              fontSize: 40,
+              fontWeight: FontWeight.w800
+          ),
+        ),
+      ),
+    );
+  }
+}
